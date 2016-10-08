70b46eadbfcba1e0444d8549911aa5c047a06d1a
tremdog <tremuljared@gmail.com>
2016-10-08 16:35:18 +1100

    Create unreleased.js

---
+++ src/data/champions/unreleased.js
@@ -0,0 +1,22 @@
+-import { CHAMPION } from '../model/Champion';		
+ -		
+ -export const UNRELEASED_CHAMPIONS = [		
+ -    // Cosmic		
+ -    CHAMPION.PHOENIX,		
+ -    CHAMPION.PHOENIXDARK,		
+ -    CHAMPION.THANOS,		
+ -    // Tech		
+ -        // Mutant		
+ -    CHAMPION.WEAPONX,		
+ -    // Skill		
+ -    CHAMPION.REDSKULL,		
+ -    // Mystic		
+ -    CHAMPION.DRSTRANGEMARVELNOW,		
+ -    CHAMPION.GHOSTRIDER,		
+ -    CHAMPION.SCARLETWITCHULTIMATE,		
+ -    // Universal		
+ -    CHAMPION.MAESTRO,		
+ -].reduce((map, champion) => {		
+ -    map[ champion ] = true;		
+ -    return map;		
+ -}, {});
